dab58ee6f8b09d9fa63ca17ccd3ede62e3fb6813
Luiz Eduardo vieira Barreto <luizbarreto@viptech.com.br>
2025-01-28 22:46:56 -0300

    feat: add search component

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header'
+import { Search } from '@/components/search'
 import { Summary } from '@/components/summary'
 import React from 'react'
 
@@ -7,10 +8,12 @@ export default function HomeLayout({ children }: React.PropsWithChildren) {
     <div className='relative py-10'>
       <div className='absolute left-0 top-0 z-[-1] h-[212px] w-full bg-gray-900' />
       <div className='mx-auto max-w-[1120px]'>
-        <div className='flex flex-col gap-y-10'>
+        <div className='mb-16 flex flex-col gap-y-10'>
           <Header />
           <Summary />
         </div>
+
+        <Search />
       </div>
       {children}
     </div>
